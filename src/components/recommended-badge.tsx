fba8241de360f20bb6fcfcb8097789385832ac8a
BeauAgst <10343831+metacurb@users.noreply.github.com>
2025-08-04 17:10:49 +0100

    chore: fix recommended badge component spacing

--- src/components/recommended-badge.tsx
+++ src/components/recommended-badge.tsx
@@ -1,9 +1,16 @@
+import clsx from "clsx";
 import { CheckBadgeIcon } from "./icons/check-badge-icon";
 
 export const RECOMMENDED_BG_COLOR = "bg-yellow-600/20";
 
-export const RecommendedBadge = () => (
-  <div className="flex items-center text-yellow-600 font-bold">
+type RecommendedBadgeProps = {
+  className?: string;
+};
+
+export const RecommendedBadge = ({ className }: RecommendedBadgeProps) => (
+  <div
+    className={clsx("flex items-center text-yellow-600 font-bold", className)}
+  >
     <CheckBadgeIcon className="mr-1" />
     Recommended
   </div>
