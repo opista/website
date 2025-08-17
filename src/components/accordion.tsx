29765401f17bae213c82423a376d8d1b0aaefea1
BeauAgst <10343831+metacurb@users.noreply.github.com>
2025-08-17 18:29:43 +0100

    chore: accordion default open prop

--- src/components/accordion.tsx
+++ src/components/accordion.tsx
@@ -2,12 +2,13 @@ import { ReactNode } from "react";
 
 type AccordionProps = {
   children: ReactNode;
+  open?: boolean;
   title: ReactNode;
 };
 
-export const Accordion = ({ children, title }: AccordionProps) => {
+export const Accordion = ({ children, open, title }: AccordionProps) => {
   return (
-    <details className="border mb-6 p-4">
+    <details className="border mb-6 p-4" open={open}>
       <summary>{title}</summary>
       <div className="mt-4">{children}</div>
     </details>
