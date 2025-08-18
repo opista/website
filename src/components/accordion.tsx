ceac8d416c29bd4500fbafd405ffbd675d7420b6
BeauAgst <10343831+metacurb@users.noreply.github.com>
2025-08-18 17:18:26 +0100

    chore: WIP styles

--- src/components/accordion.tsx
+++ src/components/accordion.tsx
@@ -8,7 +8,7 @@ type AccordionProps = {
 
 export const Accordion = ({ children, open, title }: AccordionProps) => {
   return (
-    <details className="border mb-6 p-4" open={open}>
+    <details className="border mb-5 p-4" open={open}>
       <summary>{title}</summary>
       <div className="mt-4">{children}</div>
     </details>
--- src/components/work-in-progress.tsx
+++ src/components/work-in-progress.tsx
@@ -1,17 +1,15 @@
 import { Link } from "./link";
 
-export const WorkInProgress = () => {
-  return (
-    <>
-      <p>This section is currently a work in progress.</p>
-      <p>
-        If you have experience with this area and would like to contribute,
-        please check out the{" "}
-        <Link href="#corrections-and-contributions">
-          Corrections & Contributions
-        </Link>{" "}
-        section.
-      </p>
-    </>
-  );
-};
+export const WorkInProgress = () => (
+  <div className="mb-5 px-4 py-px bg-[repeating-linear-gradient(45deg,_blue_0,_blue_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed border-blue-500">
+    <p>This section is currently a work in progress.</p>
+    <p>
+      If you have experience with this area and would like to contribute, please
+      check out the{" "}
+      <Link href="#corrections-and-contributions">
+        Corrections & Contributions
+      </Link>{" "}
+      section.
+    </p>
+  </div>
+);
