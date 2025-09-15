29528a9ed4500f84f8be21ae845d4b4b9018a5b2
BeauAgst <10343831+metacurb@users.noreply.github.com>
2025-09-15 10:42:33 +0100

    chore: add info for oxyll bt mod with KCX

---
+++ src/components/video-embed.tsx
@@ -0,0 +1,31 @@
+export type VideoEmbedProps = {
+  className?: string;
+  src: string;
+  type?: string;
+};
+
+const getFileType = (src: string) => {
+  const cleanPath = src.split(/[?#]/)[0];
+
+  if (!cleanPath) {
+    throw new Error("Invalid video path");
+  }
+
+  const parts = cleanPath?.split("/")?.pop()?.split(".");
+
+  if (!parts?.length) {
+    throw new Error("VideoEmbed expects a type prop");
+  }
+
+  return parts.pop()?.toLowerCase();
+};
+
+export const VideoEmbed = ({
+  className,
+  src,
+  type = getFileType(src),
+}: VideoEmbedProps) => (
+  <video className={className} controls>
+    <source src={src} type={`video/${type}`} />
+  </video>
+);
