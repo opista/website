0f3a03adeb80aff1c648a8c82d803f4ee8f0144b
BeauAgst <10343831+metacurb@users.noreply.github.com>
2025-10-27 12:00:33 +0000

    chore: Adds returns & warranty info to sellers

---
+++ src/components/chips/chip.tsx
@@ -0,0 +1,42 @@
+import { FC } from "react";
+import clsx from "clsx";
+
+import { IconProps } from "../icons/icon.types";
+
+type ChipProps = {
+  className?: string;
+  color?: "blue" | "green" | "red" | "yellow";
+  containerClassName?: string;
+  icon: FC<IconProps>;
+  label: string;
+};
+
+const chipColorMap: Record<NonNullable<ChipProps["color"]>, string> = {
+  blue: "bg-blue-800",
+  green: "bg-green-800",
+  red: "bg-red-800",
+  yellow: "bg-yellow-700",
+};
+
+export const Chip = ({
+  className,
+  color,
+  containerClassName,
+  icon: Icon,
+  label,
+}: ChipProps) => (
+  <div className={clsx(containerClassName, "inline-block mb-5 select-none")}>
+    <div
+      className={clsx(
+        className,
+        "inline-flex items-center rounded-full pl-1 pr-2 gap-1 text-white text-sm",
+        color ? chipColorMap[color] : chipColorMap.blue
+      )}
+    >
+      <div className="rounded-full bg-white bg-white/40 flex items-center justify-center">
+        <Icon />
+      </div>
+      <span>{label}</span>
+    </div>
+  </div>
+);
