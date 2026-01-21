bbc01bc2c488c0803741b52e7b7a114889629c32
BeauAgst <10343831+metacurb@users.noreply.github.com>
2026-01-21 13:14:31 +0000

    introduces RSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,12 @@ import { BASE_SITE_URL } from "@/constant";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  description: "Building apps to enrich your life",
+  alternates: {
+    types: {
+      "application/rss+xml": `${BASE_SITE_URL}/feed.xml`,
+    },
+  },
+  description: "Making things for fun (and sometimes profit)",
   metadataBase: new URL(BASE_SITE_URL),
   openGraph: {
     images: [
